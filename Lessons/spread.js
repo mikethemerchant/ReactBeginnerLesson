51650adf54d2ecdfe9659a30af3624f5c9dff792
mike bender <mikethemerchant@gmail.com>
2023-03-03 12:07:28 -0500

    learn the span

---
+++ Lessons/spread.js
@@ -0,0 +1,15 @@
+function stickTogether(a, b, c) {
+    return a + b + c;
+}
+
+console.log(stickTogether('what','it','is'));
+
+const list = ['eggs','milk','bread'];
+
+console.log(...list);
+
+list.forEach(x => {
+    console.log(x);
+});
+
+console.log('Good job Mike');
